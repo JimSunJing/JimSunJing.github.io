554419c78b4e1a14221d615e4a40dc1769ce5e3f
JimS <jimsun6428@gmail.com>
2022-11-16 18:09:12 +0800

    add noSQL access

--- jsj17/src/App.jsx
+++ jsj17/src/App.jsx
@@ -1,12 +1,31 @@
 import './App.css'
-
+import { useState, useEffect } from 'react';
 import ContentBlock from './components/ContentBlock'
+import * as Realm from 'realm-web'
+
 
 function App() {
+  const [mdID, setMdID] = useState(1);
+  const [blog, setBlog] = useState({md: '# Loading...'});
+  
+  useEffect(() => { async function loadMD() {
+    const REALM_APP_ID = "githubpage-0-ouxqk";
+    const app = new Realm.App({ id: REALM_APP_ID });
+    const credentials = Realm.Credentials.anonymous();
+    try {
+      const user = await app.logIn(credentials);
+      const blog = await user.functions.getOneBlog(mdID);
+      console.log(blog);
+      setBlog(blog);
+    } catch (error) {
+      console.error(error);
+    }}
+    loadMD();
+  }, [mdID]);
 
   return (
     <div className="App">
-      <ContentBlock {...testBlog}/>
+      <ContentBlock title={blog.title} md={blog.md}/>
     </div>
   )
 }
--- jsj17/src/components/ContentBlock.jsx
+++ jsj17/src/components/ContentBlock.jsx
@@ -5,10 +5,11 @@ import TextBlock from './TextBlock';
 export default function ContentBlock(props) {
 
   const html = DOMPurify.sanitize(marked.parse(props.md));
-  console.log(html);
+  // console.log(html);
 
   return (
     <div className="content-block">
+      <h1>{props.title}</h1>
       <TextBlock html={html}/>
     </div>
   )
